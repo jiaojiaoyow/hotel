c08868f4a9e9e3a130b8e220808f3080078a5b0f
shuangque <958712645@qq.com>
2019-06-30 22:27:41 +0800

    qwer

---
+++ src/main/java/com/example/hotel/dao/TroRoomMapper.java
@@ -0,0 +1,36 @@
+package com.example.hotel.dao;
+
+import com.example.hotel.model.TroRoom;
+import com.example.hotel.model.TroRoomExample;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.session.RowBounds;
+
+public interface TroRoomMapper {
+    long countByExample(TroRoomExample example);
+
+    int deleteByExample(TroRoomExample example);
+
+    int deleteByPrimaryKey(String roomintroduce);
+
+    int insert(TroRoom record);
+
+    int insertSelective(TroRoom record);
+
+    List<TroRoom> selectByExampleWithRowbounds(TroRoomExample example, RowBounds rowBounds);
+
+    List<TroRoom> selectByExample(TroRoomExample example);
+
+    TroRoom selectByPrimaryKey(String roomintroduce);
+
+    int updateByExampleSelective(@Param("record") TroRoom record, @Param("example") TroRoomExample example);
+
+    int updateByExample(@Param("record") TroRoom record, @Param("example") TroRoomExample example);
+
+    int updateByPrimaryKeySelective(TroRoom record);
+
+    int updateByPrimaryKey(TroRoom record);
+
+    List<TroRoom> selectAllTroRoom();
+}
